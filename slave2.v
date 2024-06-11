3b39842c0f7938d10fea9ae66a51581746e7da81
Samiksha-0710 <97450842+Samiksha-0710@users.noreply.github.com>
2024-06-11 14:32:48 +0530

    Add files via upload

---
+++ slave2.v
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 09.06.2024 16:59:17
+// Design Name: 
+// Module Name: slave2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module slave2(
+         input PCLK,PRESETn,
+         input PSEL,PENABLE,PWRITE,
+         input [7:0]PADDR,PWDATA,
+        output [7:0]PRDATA2,
+        output reg PREADY );
+    
+     reg [7:0]reg_addr;
+
+     reg [7:0] mem2 [0:63];
+
+    assign PRDATA2 =  mem2[reg_addr];
+
+
+
+    always @(*)
+       begin
+         if(!PRESETn)
+              PREADY = 0;
+          else
+	  if(PSEL && !PENABLE && !PWRITE)
+	     begin PREADY = 0; end
+	         
+	  else if(PSEL && PENABLE && !PWRITE)
+	     begin  PREADY = 1;
+                    reg_addr =  PADDR; 
+	       end
+          else if(PSEL && !PENABLE && PWRITE)
+	     begin  PREADY = 0; end
+
+	  else if(PSEL && PENABLE && PWRITE)
+	     begin  PREADY = 1;
+	            mem2[PADDR] = PWDATA; end
+
+           else PREADY = 0;
+        end
+    endmodule
